6bf922bbd54d55706884d60cff5dfd6b0139874a
Tkachenko AI <alexfisher13@yandex.ru>
2018-05-18 09:31:27 +0300

    add new style

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Документация React</h1>
+          <h1 className="App-title">React</h1>
         </header>
         <Tutorial/>
       </div>
